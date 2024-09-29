43783fea7597332c8e27064a37c955f5c7529e57
gabrielalb5 <gabrielalbino28@gmail.com>
2024-09-29 19:40:40 -0300

    ex1

---
+++ 09-27/meuscript.js
@@ -0,0 +1,6 @@
+console.log('Oi, mundo!');
+alert('Sua tela está sendo compartilhada');
+var nome = prompt('Seu nome:');
+var idade = parseInt(prompt('Sua idade:'));
+var proxima = idade + 1;
+alert('Olá, '+ nome + '! Você terá ' + proxima + ' anos no ano que vem.');
