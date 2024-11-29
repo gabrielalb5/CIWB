6779e6c9919c8017d9c8095a026fdb09271eb4e5
gabrielalb5 <gabrielalbino28@gmail.com>
2024-11-28 22:32:28 -0300

    .

--- carrinho/src/script.js
+++ carrinho/src/script.js
@@ -1,10 +1,10 @@
-var conteudoCarrinho = document.querySelector('#cart_products');
+let conteudoCarrinho = document.querySelector('#cart_products');
 
 if (conteudoCarrinho.children.length == 0) {
     conteudoCarrinho.innerHTML = '<p>Carrinho vazio</p>';
 }
 
-var addBtns = document.querySelectorAll('.add_to_cart');
+let addBtns = document.querySelectorAll('.add_to_cart');
 for(let btn of addBtns){
     btn.addEventListener('click',adicionarCarrinho);
 }
@@ -12,11 +12,36 @@ for(let btn of addBtns){
 function adicionarCarrinho(e){
     let li = e.target.closest('li');
     let h3 = li.querySelector('h3');
+    let p = li.querySelector('p')
+
     let productName = h3.textContent;
+    let productPrice = p.textContent;
     
     if (conteudoCarrinho.innerHTML === '<p>Carrinho vazio</p>') {
         conteudoCarrinho.innerHTML = '';
+        let tabelaCarrinho = document.createElement('table');
+        conteudoCarrinho.appendChild(tabelaCarrinho);
+        let tbody = document.createElement('tbody');
+        tabelaCarrinho.appendChild(tbody);
     }
+
+    let tbody = conteudoCarrinho.querySelector('tbody');
+    let items = tbody.querySelectorAll('tr');
+
+    for(let item of items){
+        console.log(item);
+        /*if(productName == item.querySelector('')){
+
+        }*/
+    }
+    
+
+    let tr = document.createElement('tr');
+    tr.innerHTML = `<td>${productName}</td>
+                    <td>${productPrice}</td>
+                    <td></td>
+                    <td>Remover</td>`;
+    tbody.appendChild(tr);
 
     console.log(productName);
 }
